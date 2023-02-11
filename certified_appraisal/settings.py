3c72599bbd75b153c38332e09c6722d3f68313f1
Nicolas Yeon <funyeon@gmail.com>
2023-02-11 14:55:44 -0800

    edited settings

--- certified_appraisal/settings.py
+++ certified_appraisal/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-
-SECRET_KEY= os.environ.get('SECRET_KEY')
+SECRET_KEY= str(os.getenv('SECRET_KEY'))
+# SECRET_KEY= os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
