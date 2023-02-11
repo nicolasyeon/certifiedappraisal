cad0781361840a4911f0951ac3e60c4a4ea413d1
Nicolas Yeon <funyeon@gmail.com>
2023-02-11 15:26:04 -0800

    edited settings

--- certified_appraisal/settings.py
+++ certified_appraisal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY= str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'FALSE'
 
-ALLOWED_HOSTS = ['nkyeon0709.pythonanywhere.com']
+ALLOWED_HOSTS = ['www.certifiedappraisal.net']
 
 
 # Application definition
