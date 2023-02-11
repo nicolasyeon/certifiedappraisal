fff54cebd770e44c45563b811afa75ff741e2ce4
Nicolas Yeon <funyeon@gmail.com>
2023-02-11 15:19:25 -0800

    edited settings

--- certified_appraisal/settings.py
+++ certified_appraisal/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY= str(os.getenv('SECRET_KEY'))
 # SECRET_KEY= os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = 'FALSE'
 
 ALLOWED_HOSTS = ['nkyeon0709.pythonanywhere.com']
 
